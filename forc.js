dabac540da4e23cc4eb3a368ee57eab0801a0842
tugrul95 <tugrul1995@gmail.com>
2016-03-18 10:32:39 +0200

    last lesson

---
+++ forc.js
@@ -0,0 +1,19 @@
+var elemID ="#disp";
+ /* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+                    $().ready(function()   {
+                            
+                            var top;
+                            var left;
+                            for(var i=0; i<10; i++) {
+        left = Math.floor(Math.random() * 1000) + 1);
+        top = Math.floor(Math.random() * 800 +1);
+    $(elemID).append(   "<div id='" + 1 +"' class='b' style:'top'
+\n\"</div>
+                    style=background-color=gray; top:" )
+                            }
+                        });
+
